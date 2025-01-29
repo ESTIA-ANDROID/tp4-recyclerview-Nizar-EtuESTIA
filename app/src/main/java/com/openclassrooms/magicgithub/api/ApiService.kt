03cec4c80b4f02ad1f0ffd413c1f71e02730ed9e
nizar.baddou <nizar.baddou@etu.estia.fr>
2025-01-29 14:53:02 +0100

    Fixer erreurs

---
+++ app/src/main/java/com/openclassrooms/magicgithub/api/ApiService.kt
@@ -0,0 +1,10 @@
+package com.openclassrooms.magicgithub.api
+
+import com.openclassrooms.magicgithub.model.User
+
+interface ApiService {
+    fun getUsers(): List<User>
+    fun addRandomUser()
+    fun deleteUser(username: User)
+}
+
